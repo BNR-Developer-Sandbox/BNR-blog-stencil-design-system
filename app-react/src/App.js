7a8538cd0afdb90924e534a82da293b3d6c85eeb
Richard Flosi <rflosi@bignerdranch.com>
2022-02-15 15:12:18 -0500

    Initial React app

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "design-system/ds-shell";
+import "design-system/ds-hero";
+import "design-system/ds-form";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ds-shell>
+      <h1 slot="top">React App</h1>
+      <ds-hero>
+        <ds-form>
+          <label>
+            How did you react?:
+            <br />
+            <input type="text" name="reaction" />
+          </label>
+          <br />
+          <slot slot="submit">
+            <input type="submit" value="???" />
+          </slot>
+        </ds-form>
+      </ds-hero>
+      <slot slot="bottom">
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+      </slot>
+    </ds-shell>
   );
 }
 
